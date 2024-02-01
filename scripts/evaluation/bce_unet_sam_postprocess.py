94dfe34576b1e18b3dbcad0e535ed909390d1faa
Ron Keuth <r.keuth@uni-luebeck.de>
2024-02-01 15:41:37 +0100

    add MedSAM

--- scripts/evaluation/bce_unet_sam_postprocess.py
+++ scripts/evaluation/bce_unet_sam_postprocess.py
@@ -15,40 +15,50 @@ from segment_anything.utils.prompt_utils import PromptExtractor
 from unet.classic_u_net import UNet
 from utils.dice_coefficient import multilabel_dice
 
-prompts2use1st = ["pos_points", "neg_points"]
-prompts2use2nd = ["box"]
+prompts2use1st = ["box"]
+prompts2use2nd = ["pos_points", "neg_points"]
 self_refine = True
-plot_results = True
+plot_results = False
 use_ccl = True
 
-model_id = '0427c1de20c140c5bff7284c7a4ae614'
+model_id = ['0427c1de20c140c5bff7284c7a4ae614',  # initial training
+            '0ea1c877eedc446b828048456ffd561a',  # sam pseudo labels
+            ][0]
 cl_model = InputModel(model_id)
 model = UNet.load(cl_model.get_weights(), 'cpu').eval()
-df = LightSegGrazPedWriDataset('val')
+ds = LightSegGrazPedWriDataset('val')
 
-sam_checkpoint = "data/sam_vit_h_4b8939.pth"
-model_type = "vit_h"
-device = "cpu"
-img_embedding_h5 = "data/Graz_img_embedding.h5"
-sam_predictor = SAMMaskDecoderHead(sam_checkpoint, model_type, device, img_embedding_h5)
+sam_type = ['SAM', 'MedSAM'][0]
+print(f'Using {sam_type} for refinement')
+if sam_type == 'SAM':
+    sam_checkpoint = "data/sam_vit_h_4b8939.pth"
+    sam_model_type = "vit_h"
+    img_embedding_h5 = "data/graz_sam_img_embedding.h5"
+elif sam_type == 'MedSAM':
+    sam_checkpoint = "data/medsam_vit_b.pth"
+    sam_model_type = "vit_b"
+    img_embedding_h5 = "data/graz_medsam_img_embedding.h5"
+else:
+    raise NotImplementedError(f'Unknown SAM type: {sam_type}')
+sam_predictor = SAMMaskDecoderHead(sam_checkpoint, sam_model_type, 'cpu', img_embedding_h5)
 
 if plot_results:
     dir_name = str.join('_', prompts2use1st)
     if self_refine:
         dir_name += '_self_refine_' + str.join('_', prompts2use2nd)
     dir_name += '' if use_ccl else '_no_ccl'
-    plot_save_path = Path('/home/ron/Downloads/SAM_refine_result/UNet/' + dir_name)
-    plot_save_path.mkdir(exist_ok=True)
+    plot_save_path = Path(f'/home/ron/Downloads/SAM_refine_result/{model_id}/{sam_type}/' + dir_name)
+    plot_save_path.mkdir(exist_ok=True, parents=True)
     print(f'plot_save_path: {dir_name}')
 
 dsc_unet = []
 dsc_sam = []
 dsc_sam_est = []
-for img, y, file_name in tqdm(df, unit='img'):
+for img, y, file_name in tqdm(ds, unit='img'):
     y = y.unsqueeze(0).bool()
     # forward
     with torch.inference_mode():
-        x = (img - df.IMG_MEAN) / df.IMG_STD
+        x = (img - ds.IMG_MEAN) / ds.IMG_STD
         y_hat = model(x.unsqueeze(0)).squeeze(0)
         y_hat = torch.sigmoid(y_hat) > 0.5
     unet_mask = y_hat.clone()
@@ -58,7 +68,7 @@ for img, y, file_name in tqdm(df, unit='img'):
 
     refined_sam_masks = torch.zeros_like(unet_mask, dtype=bool)
     # init with nan to avoid confusion with 0
-    est_dice = torch.full((df.N_CLASSES,), float('nan'))
+    est_dice = torch.full((ds.N_CLASSES,), float('nan'))
     for prompt in prompts:
         mask, mask_score, mask_prev_iter = sam_predictor.predict_mask(file_name, prompt, prompts2use1st)
         if self_refine:
@@ -89,7 +99,7 @@ print(f'SAM DSC: {dsc_sam.nanmean()}')
 # Dice per class
 nan_mask = dsc_unet.isnan().all(0) & dsc_sam.isnan().all(0)
 nan_mask = ~nan_mask
-plot_labels = [lbl for lbl, idx in df.BONE_LABEL_MAPPING.items() if nan_mask[idx]]
+plot_labels = [lbl for lbl, idx in ds.BONE_LABEL_MAPPING.items() if nan_mask[idx]]
 plot_dsc_nnunet = dsc_unet[:, nan_mask].nanmean(0).tolist()
 plot_dsc_sam = dsc_sam[:, nan_mask].nanmean(0).tolist()
 df = pd.DataFrame({'Anatomy': plot_labels,
--- scripts/generate_img_embeddings.py
+++ scripts/generate_img_embeddings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import cv2
 import h5py
+import numpy as np
 import torch
 from tqdm import tqdm
 
@@ -9,16 +10,23 @@ from segment_anything import sam_model_registry, SamPredictor
 
 graz_img_dir = Path('data/img_only_front_all_left').glob('*.png')
 
-# set uo SAM
-sam_checkpoint = "data/sam_vit_h_4b8939.pth"
-model_type = "vit_h"
+# set up SAM
+sam_type = ['sam', 'medsam'][1]
+print(f'Using {sam_type} model')
+if sam_type == 'sam':
+    sam_checkpoint = "data/sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+elif sam_type == 'medsam':
+    sam_checkpoint = "data/medsam_vit_b.pth"
+    model_type = "vit_b"
+
 device = "cuda:3" if torch.cuda.is_available() else "cpu"
 sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
 predictor = SamPredictor(sam)
 
 # create h5 file
-h5py_path = Path('data/Graz_img_embedding.h5')
+h5py_path = Path(f'data/graz_{sam_type}_img_embedding.h5')
 h5py_file = h5py.File(h5py_path, 'x')
 h5py_file.attrs['checkpoint'] = sam_checkpoint.split('/')[-1]
 h5py_file.attrs['img_encoder_img_size'] = sam.image_encoder.img_size
@@ -31,16 +39,37 @@ for img_file in tqdm(list(graz_img_dir), unit='img', desc='Saving embeddings'):
 
     # SAM image embedding
     with torch.inference_mode():
-        predictor.set_image(img, image_format="RGB")
+        if sam_type == 'sam':
+            predictor.set_image(img, image_format="RGB")
+            features = predictor.features.cpu().numpy()
+            original_size = predictor.original_size
+            input_size = predictor.input_size
+        elif sam_type == 'medsam':
+            img_resize = cv2.resize(
+                img,
+                (1024, 1024),
+                interpolation=cv2.INTER_CUBIC
+            )
+            # Resizing
+            img_resize = (img_resize - img_resize.min()) / np.clip(img_resize.max() - img_resize.min(), a_min=1e-8,
+                                                                   a_max=None)  # normalize to [0, 1], (H, W, 3
+            # convert the shape to (3, H, W)
+            assert np.max(img_resize) <= 1.0 and np.min(img_resize) >= 0.0, 'image should be normalized to [0, 1]'
+            img_tensor = torch.tensor(img_resize).float().permute(2, 0, 1).unsqueeze(0).to(device)
+
+            features = predictor.model.image_encoder(img_tensor).cpu().numpy()
+            original_size = img.shape[:2]
+            input_size = img_resize.shape[:2]
 
     # store to dataset
     curr_group = h5py_file.create_group(f'img_embedding/{img_file.stem}')
-    curr_group.attrs['original_size'] = predictor.original_size
-    curr_group.attrs['input_size'] = predictor.input_size
-    curr_group.create_dataset('features', data=predictor.features.cpu().numpy(), compression='gzip', compression_opts=9)
+    curr_group.attrs['original_size'] = original_size
+    curr_group.attrs['input_size'] = input_size
+    curr_group.create_dataset('features', data=features, compression='gzip', compression_opts=9)
 
     predictor.reset_image()
 
-    if c > 10: # only for testing
-        break
+    if c > 10:  # only for testing
+        pass
+        #break
 h5py_file.close()
